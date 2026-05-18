aac95cbde2ccaefc911fef083c5557f5f4f5d628
Eldoradopt <156229374+Eldoradopt@users.noreply.github.com>
2026-05-18 13:51:22 +0100

    inicio

---
+++ Proj_finalweb_loja/Proj_finalweb_loja/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Proj_finalweb_loja.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
